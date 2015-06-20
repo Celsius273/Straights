fb9806ae395d7d2ae8ddc698de35dc5164f9cd8c
DavidTSLee <lts940@naver.com>
2015-06-20 04:26:15 -0400

    argv[1]
    changed to argv[1] so we could get the seed value. now different seed =
    different randomshuffle.

--- Straights.cpp
+++ Straights.cpp
@@ -18,12 +18,12 @@ int main(int argc, char *argv[]) {
 
     int shuffleSeed = 0; //get a shuffle seed
     if (argc > 0){
-        shuffleSeed = atoi(argv[0]);
+        shuffleSeed = atoi(argv[1]);
     }
 
     // We can have a vector of players of size 4, depending on h or c for the type of player we will invoke the constructor of the corresponding player class
     vector<string> players;
-    
+
     for (int i = 1; i <= 4; i++){ // Accept h or c as inputs only, keep asking the user for input if input is not valid
         string playerType;
         cout << "Is player " << i << " a human(h) or a computer(c)?" << endl << ">";
